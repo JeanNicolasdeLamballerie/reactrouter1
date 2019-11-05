8d2ad7befa042c768ff478beee861e1da9301ae9
Jean Nicolas de Lamballerie <jean.nicolasdelamballerie@gmail.com>
2019-11-05 16:59:15 +0100

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return(
         <>
         <Link to='/notre-histoire'>To history.</Link>
+        <Link to='/'>Back to homeland.</Link>
         <p> A very useful paragraph.</p>
         <p>Bienvenue sur l'accueil.</p>
         </>)
